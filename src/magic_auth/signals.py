d345effe3a7a2c4b5d8423393f37f542f9c651ff
B4lex <b4lexxx@gmail.com>
2021-02-10 23:27:12 +0200

    Change hashing method for auth_token.

--- src/magic_auth/signals.py
+++ src/magic_auth/signals.py
@@ -1,12 +1,15 @@
 import base64
+import hashlib
 
-from django.contrib.auth.hashers import make_password
+from django.conf import settings
 
 
 def set_auth_token(**kwargs):
     instance = kwargs['instance']
     if kwargs['created']:
+        salt = settings.SECRET_KEY
+        salted_email_bytes = (instance.email + salt).encode('utf-8')
         instance.auth_token = base64.urlsafe_b64encode(
-            (make_password(instance.email) + hex(instance.id)).encode('utf-8')
+            (hashlib.sha256(salted_email_bytes).hexdigest() + hex(instance.id)).encode('utf-8')
         ).decode('utf-8')
         instance.save()
